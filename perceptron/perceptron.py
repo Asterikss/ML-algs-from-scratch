36a15fa3184d6283f8b1950c17fe2f14bdc228d6
Andre <AndreAndreM@proton.me>
2023-04-02 01:23:32 +0200

    fixes

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import logging
+import random
 
 
 class Variables:
@@ -21,55 +22,76 @@ class DefaultVariables:
 class TypeOfRead(Enum):
     TRAINING = 0
     PREDICTING = 1
-
-
-def step_func(x):
-    logging.debug(x >= 0)
-    return x >= 0
-
-
-def dot_product(X: list, weights: list) -> int:
-    return 4
     
 
 class Perceptron:
 
-    def __init__(self, learning_rate=0.01, n_iters=100) -> None:
+    def __init__(self, learning_rate=0.01, n_iters=1
+                 ) -> None:
         self.lr = learning_rate
         self.n_iters = n_iters
         self.activation_func = step_func
-        self.weights = [1, 4, 2]
-        self.bias = None
+        self.weights = [int(random.uniform(1, 4)) for _ in range(Variables.number_of_features)]
+        self.bias = -3 
 
 
-    def train(self) -> None:
+    def train(self, data_set) -> None:
         for _ in range(self.n_iters):
-            for i, x_i in enumerate(Variables.train_data):
-                output = dot_product(x_i[:-1], self.weights)
+            #for i, x_i in enumerate(Variables.train_data):
+            a = 0
+            for x_i in data_set:
+                # a+=1
+                # if a > 47:
+                    # break
+                output = dot_product(x_i[:-1], self.weights) + self.bias
                 prediction = self.activation_func(output)
 
-                update = (prediction - x_i[-1]) * self.lr 
-                logging.debug("---")
-                logging.debug(self.weights)
-                logging.debug(x_i)
-                logging.debug("---")
+                if prediction != x_i[-1]:
+                    update = (x_i[-1] - prediction) * self.lr 
+                    logging.debug("---")
+                    logging.debug(self.weights)
+                    logging.debug(x_i)
 
-                #for x, w in zip(x_i, self.weights):
-                for j, x in enumerate(x_i):
-                    #x_i[i] *= update
-                    # x *= update
-                    self.weights[j] += x * update
+                    #for x, w in zip(x_i, self.weights):
+                    for j in range(len(x_i) - 1):
+                    # for j, x in enumerate(x_i):
+                        #x_i[i] *= update
+                        # x *= update
+                        logging.debug(x_i[j])
+                        logging.debug(f"ubdate: {update}")
+                        logging.debug(x_i[j] * update)
+                        self.weights[j] += x_i[j] * update
 
-                logging.debug("---")
-                logging.debug(x_i)
-                logging.debug(self.weights)
-                logging.debug("---")
+                    for i in range(len(self.weights)):
+                        self.weights[i] = round(self.weights[i] , 3)
 
-                # for elem in self.weights:
-                #     elem += 
-        
-    def predict(self, X) -> bool:
+                    logging.debug("---")
+                    logging.debug(self.weights)
+                    logging.debug(x_i)
+                    logging.debug("---")
+                else:
+                    logging.debug("corre")
+
+
+    def predict(self, X) -> int:
         return self.activation_func(dot_product(X[:-1], self.weights))
+
+
+def step_func(x) -> int:
+    if x >= 0:
+        logging.debug("1 ret")
+        return 1
+    logging.debug("0 ret")
+    return 0
+
+
+def dot_product(X: list, weights: list) -> int:
+    result = 0
+    for x, y in zip(X, weights):
+        result += x * y
+    logging.debug(int(result))
+    return int(result)
+
 
 def ask_for_data_loc():
     missing_input = True
@@ -88,7 +110,7 @@ def ask_for_data_loc():
 
 def get_data(line: str, read_type: TypeOfRead):
     tmp_list: list = line.split()
-    logging.debug(tmp_list)
+    # logging.debug(tmp_list)
 
     # cast to tuple?
     # parsed_tmp_list = [eval(i) for i in tmp_list]
@@ -111,8 +133,7 @@ def get_data(line: str, read_type: TypeOfRead):
         parsed_tmp_list2.append(0)
 
 
-    logging.debug(parsed_tmp_list2)
-    # maby later change every chosen flower into 1 else 0
+    # logging.debug(parsed_tmp_list2)
         
     if read_type == TypeOfRead.TRAINING:
         # Variables.train_data.append(parsed_tmp_list)
@@ -139,6 +160,10 @@ def download_data_set(data_loc :str, read_type: TypeOfRead):
 
 def train():
     download_data_set(Variables.data_loc, TypeOfRead.TRAINING)
+    perceptron = Perceptron()
+    perceptron.train(Variables.train_data)
+    logging.debug(perceptron.weights)
+
 
 
 def main():
