32e2ce076041cff4246d6051deeda3fbb0e11809
Andre <AndreAndreM@proton.me>
2023-04-11 20:23:03 +0200

    init setup

--- sgl-layer-NN/sgl-layer-NN.py
+++ sgl-layer-NN/sgl-layer-NN.py
@@ -13,9 +13,54 @@ class DefaultVars:
     logging.basicConfig(level = level, format = fmt) # filename = 'log_x.log', filemode = "w"
 
 
+class Vars:
+    data_loc = ""
+    train_data = []
+
+
+class TypeOfRead(Enum):
+    TRAINING = 0
+    PREDICTING = 1
+
+
+class Layer:
+    ...
+
+
+class Neuron:
+    ...
+
+
+def ask_for_data_loc() -> str: # pure
+    missing_input = True
+    data_loc = ""
+    while missing_input:
+        answer = int(input("For default data location type 1. Otherwise type 0: "))
+        if answer == 1:
+            data_loc = "data/iris_training.txt"
+            missing_input = False
+        elif answer == 0:
+            data_loc = str(input("Enter custom data location: "))
+            data_loc = data_loc
+            missing_input = False
+        else:
+            print("Enter valid input")
+    return data_loc
+
+
+def download_data_set(data_loc: str, read_type: TypeOfRead):
+    logging.info("v")
+    logging.info("downloading data set")
+    ...
+
+def train() -> Layer:
+    download_data_set(Vars.data_loc, TypeOfRead.TRAINING)
+    return Layer()
+
 
 def main():
-    ...
+    Vars.data_loc = ask_for_data_loc()
+    layer: Layer = train()
 
 
 if __name__ == "__main__":
