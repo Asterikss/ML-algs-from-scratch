42a51b68b8b67b0ef112f2fef647d2308a652816
Andre <AndreAndreM@proton.me>
2023-04-15 15:25:59 +0200

    impl the frame

--- sgl-layer-NN/sgl-layer-NN.py
+++ sgl-layer-NN/sgl-layer-NN.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import logging
 import random
 import os
+import math
 
 
 @dataclass(frozen=True)
@@ -25,12 +26,55 @@ class TypeOfRead(Enum):
     PREDICTING = 1
 
 
-class Layer:
-    ...
+def sigmoid_func(x) -> float: # pure
+    logging.debug(f"sig: {x/(1+ math.exp(-x))}")
+    return x/(1+ math.exp(-x))
+
+
+def dot_product(X: list, weights) -> float: # pure
+    result = 0
+    for x_n, w_n in zip(X, weights):
+        result += x_n * w_n
+    logging.StreamHandler.terminator = "  "
+    logging.debug(round(result, 3))
+    logging.StreamHandler.terminator = "\n"
+    return round(result, 3)
 
 
 class Neuron:
-    ...
+    def __init__(self, n_inputs, activ_func=sigmoid_func, learning_rate=0.02) -> None:
+        self.lr: float = learning_rate
+        self.activation_func = activ_func
+        self.weights = [round(random.uniform(-1, 1), 3) for _ in range(n_inputs)]
+        self.bias = round(random.uniform(-1, 1), 3)
+
+
+    def output(self, X) -> float:
+        return self.activation_func(dot_product(X[:-1], self.weights) + self.bias)
+
+
+class Layer:
+    def __init__(self, n_inputs, n_neurons) -> None:
+        self.neurons: list[Neuron] = [Neuron(n_inputs) for _ in range(n_neurons)]
+
+    
+    def feed_forward(self, X) -> list[float]:
+        outputs = []
+        for neuron in self.neurons:
+            outputs.append(neuron.output(X)) 
+        return outputs
+
+
+class NeuralNetwork():
+    def __init__(self, n_inputs, layers: list[int]) -> None:
+        self.layers = [Layer(n_inputs, layers[0])]
+        self.layers += [Layer(layers[i], layers[i+1]) for i in range(len(layers) - 1)]
+
+
+    def train(self, train_data: list[list[int]]):
+        example_one = train_data[0]
+        self.layers[0].feed_forward(example_one)
+        ...
 
 
 def ask_for_data_loc() -> str: # pure
@@ -44,7 +88,7 @@ def ask_for_data_loc() -> str: # pure
             print("Enter valid input")
 
 
-def download_data_set(root_directory: str, read_type: TypeOfRead) -> list[list[int]]:
+def download_data_set(root_directory: str) -> list[list[int]]: # pure
     logging.info("v")
     logging.info("downloading data set")
     collected_data = []
@@ -79,14 +123,22 @@ def download_data_set(root_directory: str, read_type: TypeOfRead) -> list[list[i
     return collected_data
 
 
-def train() -> Layer:
-    Vars.train_data = download_data_set(Vars.data_loc, TypeOfRead.TRAINING)
-    return Layer()
+# def init_layer():
+#     return Layer(26)
+
+
+# def train(data_loc: str) -> NeuralNetwork:
+#     train_data = download_data_set(data_loc)
+#     neural_network: NeuralNetwork = 
+#     return NeuralNetwork([3])
 
 
 def main():
-    Vars.data_loc = ask_for_data_loc()
-    layer: Layer = train()
+    data_loc = ask_for_data_loc()
+    train_data = download_data_set(data_loc)
+    neural_network: NeuralNetwork = NeuralNetwork(26, [3])
+    logging.debug(neural_network)
+    neural_network.train(train_data)
 
 
 if __name__ == "__main__":
