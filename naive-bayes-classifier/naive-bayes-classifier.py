eb17791f42ab79b6fa47f562a9a2ad04157bceaa
Andre <AndreAndreM@proton.me>
2023-05-02 14:11:45 +0200

    impl min max for features

--- naive-bayes-classifier/naive-bayes-classifier.py
+++ naive-bayes-classifier/naive-bayes-classifier.py
@@ -17,14 +17,44 @@ def ask_for_data_loc() -> pathlib.Path: # ~~pure
 def downlad_dataset(data_loc: pathlib.Path) -> tuple[list[list[float]], int, list[str], list[int]]: # pure
     collected_data = []
     label_tabel = []
-    # not using a dict since indexes matter
+    # ^, v - not using a dict since indexes matter
     label_occurrence_tabel = []
+
+    #           [
+    # feature_n   [min, max]
+    # feature_n+1 [min, max]
+    # ...
+    #                      ]
+    min_and_max_table: list[list[float]] = []
+    firstTime = True
     
 
     with open(data_loc, "r", encoding="utf-8") as f:
         for line in f:
             splited: list[str] = line.split()
-            decoded: list[float] = [eval(splited[i]) for i in range(len(splited) - 1)]
+            # decoded2: list[float] = [eval(splited[i]) for i in range(len(splited) - 1)]
+            if firstTime:
+                    min_and_max_table: list[list[float]] = [[0,0] for _ in range(len(splited) - 1)]
+            
+            decoded: list[float] = []
+            for i in range(len(splited) - 1):
+                tmp_eval = eval(splited[i])
+                decoded.append(tmp_eval)
+
+                if firstTime:
+                    min_and_max_table[i][0] = tmp_eval
+                    min_and_max_table[i][1] = tmp_eval
+                
+
+                if not firstTime:
+                    if tmp_eval < min_and_max_table[i][0]:
+                        min_and_max_table[i][0] = tmp_eval
+                    elif tmp_eval > min_and_max_table[i][1]:
+                        min_and_max_table[i][1] = tmp_eval
+
+            firstTime = False
+
+
 
             label = splited[-1]
             if label not in label_tabel:
@@ -42,6 +72,7 @@ def downlad_dataset(data_loc: pathlib.Path) -> tuple[list[list[float]], int, lis
     logging.info(f"Label table: {label_tabel}")
     logging.info(f"Label occurrence table: {label_occurrence_tabel}")
     logging.info(f"Length of the dataset: {len(collected_data)}")
+    logging.info(f"min and max table: {min_and_max_table}")
 
     return collected_data, number_of_feature, label_tabel, label_occurrence_tabel
 
